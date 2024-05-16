7487892c87fe1b846d8205bbb2029f9d740412ff
David Lysenko <lysenkodavid@gmail.com>
2024-05-16 22:31:57 +0200

    Implement ManagedMemory

---
+++ src/managed_memory.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
+const expect = std.testing.expect;
+
+pub const ManagedMemory = struct {
+    const Self = @This();
+
+    backing_allocator: Allocator,
+    bytes_allocated: usize = 0,
+    is_gc_active: bool = false,
+
+    pub fn init(backing_allocator: Allocator) Self {
+        return .{
+            .backing_allocator = backing_allocator,
+        };
+    }
+
+    pub fn allocator(self: *Self) Allocator {
+        return .{
+            .ptr = self,
+            .vtable = &.{
+                .alloc = alloc,
+                .resize = resize,
+                .free = free,
+            },
+        };
+    }
+
+    fn alloc(
+        ctx: *anyopaque,
+        len: usize,
+        ptr_align: u8,
+        ret_addr: usize,
+    ) ?[*]u8 {
+        const self: *Self = @ptrCast(@alignCast(ctx));
+        const out_opt = self.backing_allocator.rawAlloc(
+            len,
+            ptr_align,
+            ret_addr,
+        );
+
+        if (out_opt != null) {
+            self.bytes_allocated += len;
+        }
+
+        return out_opt;
+    }
+
+    fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+        const self: *Self = @ptrCast(@alignCast(ctx));
+        const is_same_address = self.backing_allocator.rawResize(
+            buf,
+            buf_align,
+            new_len,
+            ret_addr,
+        );
+
+        if (is_same_address) {
+            if (new_len > buf.len) {
+                self.bytes_allocated += new_len - buf.len;
+            } else {
+                self.bytes_allocated -= buf.len - new_len;
+            }
+        }
+
+        return is_same_address;
+    }
+
+    fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
+        const self: *Self = @ptrCast(@alignCast(ctx));
+
+        self.backing_allocator.rawFree(buf, buf_align, ret_addr);
+        self.bytes_allocated -= buf.len;
+    }
+};
+
+test "should track memory correctly on alloc and free" {
+    // GIVEN
+    var managed_memory = ManagedMemory.init(std.testing.allocator);
+    const allocator = managed_memory.allocator();
+
+    // WHEN - THEN
+    try expect(managed_memory.bytes_allocated == 0);
+
+    const buf = try allocator.alloc(u8, 8);
+
+    try expect(managed_memory.bytes_allocated == 8);
+
+    allocator.free(buf);
+
+    try expect(managed_memory.bytes_allocated == 0);
+}
+
+test "should track memory correctly on realloc" {
+    // GIVEN
+    var managed_memory = ManagedMemory.init(std.testing.allocator);
+    const allocator = managed_memory.allocator();
+
+    // WHEN - THEN
+    var buf = try allocator.alloc(u8, 8);
+
+    try expect(managed_memory.bytes_allocated == 8);
+
+    buf = try allocator.realloc(buf, 32);
+
+    try expect(managed_memory.bytes_allocated == 32);
+
+    buf = try allocator.realloc(buf, 8);
+
+    try expect(managed_memory.bytes_allocated == 8);
+
+    allocator.free(buf);
+
+    try expect(managed_memory.bytes_allocated == 0);
+}
