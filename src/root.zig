afa34114388972f2251d633c034f703417bab5d4
David Lysenko <lysenkodavid@gmail.com>
2025-02-09 00:24:33 +0100

    Remove direct imports from bin

--- src/root.zig
+++ src/root.zig
@@ -6,6 +6,9 @@ const managed_memory_mod = @import("state/managed_memory.zig");
 const compiler_mod = @import("compiler/compiler.zig");
 const vm_mod = @import("vm/vm.zig");
 const error_reporter_mod = @import("reporter/error_reporter.zig");
+const debug_ast_reporter_mod = @import("reporter/debug_ast_reporter.zig");
+const debug_reporter_mod = @import("reporter/debug_reporter.zig");
+const obj_mod = @import("state/obj.zig");
 
 pub const Token = tokenizer_mod.Token;
 pub const Tokenizer = tokenizer_mod.Tokenizer;
@@ -17,4 +20,7 @@ pub const SemaType = sema_ast_mod.SemaType;
 pub const ManagedMemory = managed_memory_mod.ManagedMemory;
 pub const Compiler = compiler_mod.Compiler;
 pub const Vm = vm_mod.Vm;
+pub const Obj = obj_mod.Obj;
 pub const error_reporter = error_reporter_mod;
+pub const debug_ast_reporter = debug_ast_reporter_mod;
+pub const debug_reporter = debug_reporter_mod;
